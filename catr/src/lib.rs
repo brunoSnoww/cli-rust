fc102660c3c72cff80209e90171c106cf80cecf9
Bruno Neves <bruno.neves@issuu.com>
2024-10-24 14:40:15 +0100

    catr

---
+++ catr/src/lib.rs
@@ -0,0 +1,90 @@
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+use clap::{App, Arg};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_noblank_lines: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("catr")
+        .version("0.1.0")
+        .author("Bruno F Neves")
+        .about("A dude")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .help("Number lines")
+                .takes_value(false)
+                .conflicts_with("number_noblank"),
+        )
+        .arg(
+            Arg::with_name("number_noblank")
+                .short("b")
+                .long("number-noblank")
+                .help("Number non-blank lines")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    let files = matches.values_of_lossy("files").unwrap();
+    let number_lines = matches.is_present("number");
+    let number_noblank_lines = matches.is_present("number_noblank");
+
+    Ok(Config {
+        files,
+        number_lines,
+        number_noblank_lines,
+    })
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        " " => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("{}:{}", filename, err),
+            Ok(file) => {
+                let mut last_num = 0;
+                for (line_num, line) in file.lines().enumerate() {
+                    let line = line?;
+                    if config.number_lines {
+                        println!("{:>6}\t{}", line_num + 1, line);
+                    } else if config.number_noblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:>6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
